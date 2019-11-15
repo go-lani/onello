7a5f6fa3026adeb83cb4bba4652d3c046a9b674b
publizm <publizm@gmail.com>
2019-11-15 23:44:37 +0900

    feat: complete drag and drop ui

--- public/assets/js/dragdrop.js
+++ public/assets/js/dragdrop.js
@@ -1,8 +1,8 @@
-function dragStart(e) {
-  console.warn('dragStart');
-  const parentNode = e.target.parentElement;
-  e.dataTransfer.setDragImage(parentNode,0,0);
-  e.dataTransfer.setData('targetId', e.target.parentElement.id);
+function onDragStart(event) {
+  const id = event.target.parentNode.id;
+
+  event.dataTransfer.setDragImage(event.target.parentNode,145,47);
+  event.dataTransfer.setData('text/plain', id);
 }
 
 function drag(e) {
@@ -14,4 +14,23 @@ function drop(e) {
   const targetId = e.dataTransfer.getData('targetId');
   e.preventDefault();
   e.target.appendChild(document.getElementById(targetId));
+}
+
+function onDrop(event) {
+  const id = event.dataTransfer.getData('text');
+
+  console.log(id);
+
+  const draggableElement = document.getElementById(id);
+
+  const dropzone = event.target.parentNode.parentNode.parentNode;
+
+  if (dropzone.classList.contains('detail-work')) {
+    dropzone.appendChild(draggableElement);
+    event.dataTransfer.clearData();
+  };
+}
+
+function onDragOver(event) {
+  event.preventDefault();
 }
