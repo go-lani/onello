89e9493f344a4bc0e6738c9915095e20327d33da
publizm <publizm@gmail.com>
2019-11-17 20:45:44 +0900

    docs: popup styling complete

--- public/assets/js/popup.js
+++ public/assets/js/popup.js
@@ -9,62 +9,71 @@ const render = () => {
 
   $node.innerHTML += `
     <div class="register-popup">
-    <div class="popup-header">
-      <div class="popup-title"><span class="a11y-hidden">팝업타이틀:</span>11/08 프로젝트 주제 회의</div>
-      <div class="popup-subtitle">in list<a href="#self">회의 내용</a></div>
-      <div class="popup-create-time">19/01/01 12:12:12</div>
-    </div>
-
-    <button type="button" class="btn-close-popup">X</button>
-
-    <div class="popup-main-content">
-      <div class="description-area">
-        <div class="area-title">Description</div>
-        <textarea class="description-content" placeholder="Add a more detailed Description.."></textarea>
-        <button type="button" class="btn-save">Save</button>
+      <div class="popup-header">
+        <div class="popup-title"><span class="a11y-hidden">주제:</span>11/08 프로젝트 주제 회의</div>
+        <div class="popup-subtitle">in list <a href="#self">회의 내용</a></div>
+        <div class="popup-created-time">19/01/01 12:12:12</div>
       </div>
 
-      <div class="checklist-area">
-        <div class="area-title">checklist</div>
-        <div class="progress-contents">
-          <span class="complete-percent">98%</span>
-          <div class="progress-bar">
-            <span class="success-bar"></span>
+      <button type="button" class="btn-close-popup layer-close">X</button>
+
+      <div class="popup-main-content clear-fix">
+        <div class="content-area">
+          <div class="description-area">
+            <div class="area-title">Description</div>
+            <textarea class="description-content" placeholder="Add a more detailed Description.."></textarea>
+            <button type="button" class="btn-save btn40 c5 mt10" style="width: 80px;">Save</button>
+          </div>
+
+          <div class="checklist-area">
+            <div class="area-title">checklist</div>
+            <div class="progress-contents">
+              <span class="complete-percent">98%</span>
+              <div class="progress-bar">
+                <span class="success-bar" style="width: 50%"></span>
+              </div>
+            </div>
+            <ul class="check-list">
+              <li><label class="chk" for="check1"><input id="check1" type="checkbox"><span>ddasda</span></label></li>
+              <li><label class="chk" for="check2"><input id="check2" type="checkbox"><span>ddasda</span></label></li>
+            </ul>
+            <button type="button" class="btn-check-add btn40 c5 mt20" style="width: 120px;">add an item</button>
+            <button type="button" class="btn-delete btn30 c6" style="width: 100px;">delete</button>
           </div>
         </div>
-        <ul>
-          <li><label for="check1"><input id="check1" type="checkbox"><span>ddasda</span></label></li>
-          <li><label for="check2"><input id="check2" type="checkbox"><span>ddasda</span></label></li>
-        </ul>
-        <button type="button" class="btn-check-add">add an item</button>
-        <button type="button" class="btn-delete">delete</button>
-      </div>
-    </div>
 
-    <div class="popup-add-ons">
-      <div class="members-info">
-        <div class="title">MEMBERS</div>
-        <ul class="members-list">
-          <l1>P</l1>
-          <l1>정</l1>
-          <l1>김</l1>
-          <l1>+</l1>
-        </ul>
-      </div>
-      <div class="labels">
-        <div class="title">LABELS</div>
-        <ul class="colors-list">
-          <li class="none"><span class="a11y-hidden">none</span></li>
-          <li class="green"><span class="a11y-hidden">green</span></li>
-          <li class="red"><span class="a11y-hidden">red</span></li>
-          <li class="blue"><span class="a11y-hidden">blue</span></li>
-        </ul>
-      </div>
-      <div class="check">
-        <button type="button" class="btn-checklist">CHECKLIST</button>
-      </div>
-    </div>
-  </div>`;
+        <div class="popup-add-ons">
+          <div class="members-info">
+            <div class="title">MEMBERS</div>
+            <ul class="members-list">
+              <li>P</li>
+              <li>정</li>
+              <li>김</li>
+              <li>+</li>
+            </ul>
+          </div>
+          <div class="labels">
+            <div class="title">LABELS</div>
+            <ul class="colors-list">
+              <li class="yellow">
+                <label><input type="checkbox" class="state-check"><span>노랑색</span></span></label>
+              </li>
+              <li class="orange">
+                <label><input type="checkbox" class="state-check"><span>주황색</span></span></label>
+              </li>
+              <li class="red">
+                <label><input type="checkbox" class="state-check"><span>빨강색</span></span></label>
+              </li>
+              <li class="blue">
+                <label><input type="checkbox" class="state-check"><span>파랑색</span></span></label>
+              </li>
+            </ul>
+          </div>
+          <div class="add-check">
+            <button type="button" class="btn-checklist btn40 c2" style="width: 100%;">CHECKLIST</button>
+          </div>
+        </div>
+      </div>`;
 
   $wrap.appendChild($node);
 };
@@ -111,8 +120,10 @@ const openPopup = () => {
 };
 
 // Events
-$mainwork.addEventListener('click', (e) => {
-  if (!e.target.parentNode.classList.contains('detail-inner')) return;
+// $mainwork.addEventListener('click', (e) => {
+//   if (!e.target.parentNode.classList.contains('detail-inner')) return;
 
-  openPopup();
-});
+//   openPopup();
+// });
+
+window.onload = openPopup;
